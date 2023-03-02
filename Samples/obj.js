840d9c8acb3d04be61f6277a4a76b946b135e355
udhayaabisheik <n.u.abisheik2001@gmail.com>
2023-03-02 11:42:08 +0530

    working with objects and notations

---
+++ Samples/obj.js
@@ -0,0 +1,11 @@
+let person = {
+    firstname : "Udhaya",
+    lastname : "abisheik",
+    age : 22,
+    city : "Tirunelveli",
+    "1" : "one",
+    "my choice" : "Developer"
+}
+console.log(person.firstname,person.lastname);//dot notation
+console.log(person["1"]);//bracket notaion
+console.log(person["my choice"]);
